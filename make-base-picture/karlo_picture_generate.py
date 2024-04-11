a4a625a37386dbefceda2da43a5230483ab865ce
SChaeck <zmfhtmgjsej@dgu.ac.kr>
2024-04-11 09:56:53 +0900

    base-picuture 및 라벨링 검수 완료. 최종 검수 필요. base-picture 한 번에 모두 생성하는 기능 구현

--- make-base-picture/karlo_picture_generate.py
+++ make-base-picture/karlo_picture_generate.py
@@ -4,12 +4,14 @@ import json
 import urllib
 import os
 import json
+import threading
+from tqdm import tqdm
 from PIL import Image
 
-classic_template_subjects = ["mountain1", "mountain2", "park1", "park2", "sky1", "sky2"] # 여기에 넣은 subject는 기존 프롬프트 사용
+classic_template_subjects = ["mountain", "park", "sky"] # 여기에 넣은 subject는 기존 프롬프트 사용
 CLASSIC_NEGATIVE_PROMPT = 'scary, a dividing line, darkness, person, human-like, complicated, stack, small creature, an alcoholic beverage, a moon, dirty, crowded, faint, ambiguous, box'
 
-memory_template_subjects = ["stream1", "stream2", "farming1", "farming2"] # 새로운 프롬프트 사용
+memory_template_subjects = ["stream", "farming"] # 새로운 프롬프트 사용
 MEMORY_NEGATIVE_PROMPT = 'out of frame, low resolution, blurry, worst quality, fuzzy, lowres, text, low quality, signature, grainy, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, body out of frame, watermark, distorted face, bad anatomy, missing anatomy, missing body, missing face, missing legs, missing fingers, missing feet, missing toe, fewer digits, extra limbs, extra anatomy, extra face, extra arms, extra fingers, extra hands, extra legs, extra feet, extra toe, mutated hands, ugly, mutilated, disfigured, mutation, bad proportions, cropped head, cross-eye, mutilated, distorted eyes, strabismus, skin blemishes, Japan, China, Japanese, Chinese, Japanese language, Chinese language, Southeast Asia'
 
 #%% API 키 불러오기
@@ -68,6 +70,10 @@ def make_prompt(subject):
     # 키워드 수에 맞게 자동으로 and 조절
     words_placeholder = ' and '.join(['{' + f'word{i+1}' + '}' for i in range(len(keywords))])
 
+    # 주제 뒤에 붙은 버전 전처리
+    if subject[-1].isdigit():
+        subject = subject[:-1]
+
     # classic과 memory를 구분하여 프롬프트 생성
     if subject in classic_template_subjects:
         prompt_template = f"clear style, appropriate distance between objects, purest form of minimalistic perfection, the {subject}-themed {words_placeholder}, high-end graphic illustration, high contrast, realistic colors."
@@ -82,23 +88,56 @@ def make_prompt(subject):
 
     return (prompt, negative_prompt)
 
-if __name__ == "__main__":
-    # 만들어낼 그림 주제
-    """ 
-    subject: "mountain1", "mountain2", "park1", "park2", "sky1", "sky2"
-    final-subject: "stream1", "stream2", "farming1", "farming2"
-    """
-    subject = "farming2"
-
+def process_subject(subject, progress):   
     # 프롬프트에 사용할 제시어
     prompt = make_prompt(subject)
-    print(prompt)
 
     # 이미지 생성하기 REST API 호출
     response = t2i(prompt)
 
-    # 응답의 첫 번째 이미지 생성 결과 출력하기
+    # 응답의 첫 번째 이미지 생성 결과 추출
     result = Image.open(urllib.request.urlopen(response.get("images")[0].get("image")))
-    result.show()
 
-    result.save(f'make-base-picture/final-base-picture/{subject}-base-picture.png','PNG') 
+    # 결과 이미지 저장
+    result.save(f'make-base-picture/base-picture/{subject}-base-picture.png', 'PNG')
+    progress.update(1)
+
+def make_all_base_picture():
+    subject_list = ['mountain1', 'mountain2', 'park1', 'park2', 'sky1', 'sky2', 'stream1', 'stream2', 'farming1', 'farming2']
+    
+    # 진행 상태를 업데이트하기 위한 tqdm 객체 생성
+    progress = tqdm(total=len(subject_list))
+
+    # 각 주제에 대해 별도의 스레드 생성 및 시작
+    threads = []
+    for subject in subject_list:
+        thread = threading.Thread(target=process_subject, args=(subject, progress))
+        threads.append(thread)
+        thread.start()
+
+    # 모든 스레드가 완료될 때까지 대기
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    make_all_base_picture() # 모든 베이스 그림 전부 재생성
+
+    # # 만들어낼 그림 주제
+    # """ 
+    # subject: "mountain1", "mountain2", "park1", "park2", "sky1", "sky2"
+    # final-subject: "stream1", "stream2", "farming1", "farming2"
+    # """
+    # subject = "stream2"
+
+    # # 프롬프트에 사용할 제시어
+    # prompt = make_prompt(subject)
+    # print(prompt)
+
+    # # 이미지 생성하기 REST API 호출
+    # response = t2i(prompt)
+
+    # # 응답의 첫 번째 이미지 생성 결과 출력하기
+    # result = Image.open(urllib.request.urlopen(response.get("images")[0].get("image")))
+    # result.show()
+
+    # result.save(f'make-base-picture/base-picture/{subject}-base-picture.png','PNG') 
--- make-base-picture/server_use_karlo.py
+++ make-base-picture/server_use_karlo.py
@@ -2,8 +2,10 @@ import urllib
 from karlo_picture_generate import make_prompt, t2i
 from PIL import Image
 
-
-# subject: classroom | park | mountain | beach | room
+""" 
+subject: "mountain1", "mountain2", "park1", "park2", "sky1", "sky2"
+final-subject: "stream1", "stream2", "farming1", "farming2"
+"""
 subject = "sky"
 prompt = make_prompt(subject)
 print(prompt)
