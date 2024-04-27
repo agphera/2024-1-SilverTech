e75d8efeb8da67d204320baabc66a329346443fa
SChaeck <zmfhtmgjsej@dgu.ac.kr>
2024-04-27 19:47:28 +0900

    단어 유사도 API 오류 해결
    문제상황 - 멀티스레딩으로 여러 request를 날리면 순간적으로 제한에 걸림 -> 예상하지 못한 object가 반환됨
    
    해결 - 올바른 response가 올 때까지 반복해서 request 요청

--- Frontend_UI/index.js
+++ Frontend_UI/index.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let container = document.querySelector('.navbar-progress-container');
   let progressBar = document.querySelector('progress');
   let popup = document.getElementById("analysisPopup");
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
   //let imagePopup = document.getElementById("imagePopup"); // 비교이미지 팝업
 
   /*시간 설정*/
-  let duration = 15000; // 프로그레스바 15초
+  let duration = 11000; // 프로그레스바 15초
   let hideDuration = 30000; // navbar+progressbar 사라지는 시간 30초(변수만 설정, 아직 미구현)
   let popupDuration = 15000; // 분석중입니다 팝업시간 15초
 
@@ -30,13 +30,13 @@ document.addEventListener("DOMContentLoaded", function() {
     container.style.opacity = 0;
 
     popup.style.display = "flex"; // analysis popup 보여주기
-    setTimeout(function() {
+    setTimeout(function () {
       popup.style.opacity = 1; // 부드러운 효과
     }, 10);
 
-    setTimeout(function() {
+    setTimeout(function () {
       popup.style.opacity = 0; // 부드러운 효과
-      setTimeout(function() {
+      setTimeout(function () {
         popup.style.display = "none"; // analaysis popup 사라지기
         //imagePopup.style.display = "block"; // Show image popup
         showElements(); //navbar+progressbar 다시 보여주는 함수
@@ -54,23 +54,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 //모든 학습파일
-function visible(current, total){
+function visible(current, total) {
   var arr = [];
-  for(let k = 0; k< total; k++){
-    arr[k] = k+"text";
+  for (let k = 0; k < total; k++) {
+    arr[k] = k + "text";
   }
   var con = document.getElementById(arr[current]);
-  for(let j = 0; j<arr.length; j++){
+  for (let j = 0; j < arr.length; j++) {
     var hid = document.getElementById(arr[j]);
     hid.style.display = 'none';
-  }	
-  con.style.display = 'block'; 	
-  
+  }
+  con.style.display = 'block';
+
 }
 
 
 
-console.log("보내기");console.log("보내기");console.log("보내기");console.log("보내기");
+console.log("보내기"); console.log("보내기"); console.log("보내기"); console.log("보내기");
 
 
 
@@ -83,23 +83,23 @@ console.log("보내기");console.log("보내기");console.log("보내기");conso
 
 
 //모든 학습파일
-function visible(current, total){
+function visible(current, total) {
   var arr = [];
-  for(let k = 0; k< total; k++){
-    arr[k] = k+"text";
+  for (let k = 0; k < total; k++) {
+    arr[k] = k + "text";
   }
   var con = document.getElementById(arr[current]);
-  for(let j = 0; j<arr.length; j++){
+  for (let j = 0; j < arr.length; j++) {
     var hid = document.getElementById(arr[j]);
     hid.style.display = 'none';
-  }	
-  con.style.display = 'block'; 	
-  
+  }
+  con.style.display = 'block';
+
 }
 
 
 
-console.log("보내기");console.log("보내기");console.log("보내기");console.log("보내기");
+console.log("보내기"); console.log("보내기"); console.log("보내기"); console.log("보내기");
 
 
 
