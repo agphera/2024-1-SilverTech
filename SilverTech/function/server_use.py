20de204478242134f32e5982270229fd04c7b4d6
SChaeck <zmfhtmgjsej@dgu.ac.kr>
2024-05-04 23:18:51 +0900

    전체적인 API 코드 최적화

--- SilverTech/function/server_use.py
+++ SilverTech/function/server_use.py
@@ -11,13 +11,13 @@ def scoring_points(data):
     print(f"입력 받은 문장: {INPUT_TEXT}")
 
 
-    #2 키워드 추출
+    #2 명사(키워드) 추출
     results = sbg_noun_extractor(INPUT_TEXT)
     print(f"키워드: {results}")
 
 
     #3 유사도 측정으로 점수 결정
-    true_word, false_word, accuracy, whole_prompt = user_base_similarity(THEMA, results)
+    true_word, false_word, accuracy, whole_prompt_word = user_base_similarity(THEMA, results)
     print("정답 키워드 개수:", len(true_word)) 
     print("정답 단어:", true_word)
     print("정답률:", accuracy)
@@ -27,18 +27,21 @@ def scoring_points(data):
     #4 영어로 번역
     false_word_trans = translate_text_list(false_word) #틀린 단어만 번역
     print(f"번역한 오답 단어: {false_word_trans}")
-    whole_prompt = whole_prompt.union(false_word_trans)
 
-    return accuracy, true_word, whole_prompt
+    #5 프롬프트에 들어갈 전체 단어 모음
+    whole_prompt_word = whole_prompt_word.union(false_word_trans)
 
-def make_picture(whole_prompt):
+    return accuracy, true_word, whole_prompt_word
+
+def make_picture(whole_prompt_word):
     THEMA = "park1"
     
-    #5 그림 생성
-    print(whole_prompt)
-    prompt = make_prompt(THEMA, list(whole_prompt))
+    #1 프롬프트 생성
+    print(whole_prompt_word)
+    prompt = make_prompt(THEMA, list(whole_prompt_word))
     print(f"생성한 프롬프트: {prompt}")
 
+    #2 그림 생성
     response = t2i(prompt)
     print(response)
 
