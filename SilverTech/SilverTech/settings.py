bd379f761f88352c6cc14ab604777cf288b71f0d
SChaeck <zmfhtmgjsej@dgu.ac.kr>
2024-06-04 20:50:46 +0900

    서버에서 다른 앱 사이에 세션 공유하도록 settings.py 변경

--- SilverTech/SilverTech/settings.py
+++ SilverTech/SilverTech/settings.py
@@ -167,3 +167,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
 
 # MEDIA_URL 설정
 MEDIA_URL = '/Media/'
+
+# 다른 앱 사이에서 세션 공유
+SESSION_COOKIE_AGE = 10800  # 세션 쿠키의 유효기간 (3시간)
+SESSION_COOKIE_DOMAIN = '.cogpicture.duckdns.org'  # '.yourdomain.com'으로 변경
+SESSION_COOKIE_SECURE = True  # HTTPS 사용 시 True로 설정
+SESSION_COOKIE_NAME = 'sessionid'  # 세션 쿠키의 이름
