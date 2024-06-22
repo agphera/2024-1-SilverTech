3850ec0e1390e612a9d8f5da5fab324c77f12750
SChaeck <zmfhtmgjsej@dgu.ac.kr>
2024-06-22 15:57:59 +0900

    로그인/회원가입 가능하게 서버 버전 주석처리 + 필요없는 파일 일부 정리

--- SilverTech/SilverTech/settings.py
+++ SilverTech/SilverTech/settings.py
@@ -174,7 +174,7 @@ MEDIA_URL = '/Media/'
 SESSION_COOKIE_AGE = 10800  # 세션 쿠키의 유효기간 (3시간)
 
 ## 여기 아래서부턴 서버 업로드용
-SESSION_COOKIE_DOMAIN = '.cogpicture.duckdns.org'  # '.yourdomain.com'으로 변경
-SESSION_COOKIE_SECURE = True  # HTTPS 사용 시 True로 설정
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_NAME = 'sessionid'  # 세션 쿠키의 이름
+# SESSION_COOKIE_DOMAIN = '.cogpicture.duckdns.org'  # '.yourdomain.com'으로 변경
+# SESSION_COOKIE_SECURE = True  # HTTPS 사용 시 True로 설정
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_NAME = 'sessionid'  # 세션 쿠키의 이름
